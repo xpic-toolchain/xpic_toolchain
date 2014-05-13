c74b2117460199e2ac405acc2bb30e0f6972399b
danielhil <danielhil@5227c84a-046b-4545-895e-fca5577db8e1>
2014-05-13 12:20:58 +0000

    offline toolchain workflow tests added

---
+++ trunk/testsuite/latest/offline_tests/toolxpic-5_with_workaround/test_sources/some.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "some.h"
+
+unsigned long somelongarray[4000] = {
+0x12345678
+};
+
+unsigned long somelongarray2[70000] = { 0x12345678 };
+
+int some( void )
+{
+  somelongarray[1] = longarray2[68000] + somelongarray[500] + somelongarray2[69000];
+  
+  return 0;
+}
