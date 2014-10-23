4910204163378dfe587a2705a3438e21d00353fa
helmutmanck <helmutmanck@5227c84a-046b-4545-895e-fca5577db8e1>
2014-10-23 15:24:08 +0000

    revert and hasReservedCallFrame == false

--- llvm-3.4.2/lib/Target/xpic/xpicFrameLowering.cpp
+++ llvm-3.4.2/lib/Target/xpic/xpicFrameLowering.cpp
@@ -60,6 +60,10 @@ bool xpicFrameLowering::hasFP(const MachineFunction &MF) const {
   return false;
 }
 
+bool xpicFrameLowering::hasReservedCallFrame(const MachineFunction &MF) const {
+  return false;
+}
+
 void xpicFrameLowering::eliminateCallFramePseudoInstr(MachineFunction &MF, MachineBasicBlock &MBB, MachineBasicBlock::iterator I) const {
 #ifdef DEBUG_SHOW_FNS_NAMES
 printf("xpicFrameLowering::eliminateCallFramePseudoInstr\n");
