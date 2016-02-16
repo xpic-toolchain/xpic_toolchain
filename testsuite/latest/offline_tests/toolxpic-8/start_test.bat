a2d4dd2f5bf7b7a096cb2157d00aa1bd81176a33
gdormann <gdormann@hilscher.com>
2016-02-16 11:02:28 +0100

    change compiler name for offline tests

--- testsuite/latest/offline_tests/toolxpic-8/start_test.bat
+++ testsuite/latest/offline_tests/toolxpic-8/start_test.bat
@@ -1,6 +1,7 @@
 @echo off
 cd test_sources
-%PATH_GNU_XPIC%\bin\xpic-llvmc -O0 -Wa,-mmcu=xpic2 main.c
+%PATH_GNU_XPIC%\bin\clang -o0 -target xpic -Wimplicit  -T linker.ld main.c
+REM %PATH_GNU_XPIC%\bin\xpic-llvmc -O0 -Wa,-mmcu=xpic2 main.c
 if %ERRORLEVEL%==0 (
 echo test was successful.
 ) else (
