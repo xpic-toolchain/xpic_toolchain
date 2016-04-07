ee297bd2823328a51b9aab67a1650f7bf4a60e14
Martin Bober <martin.bober@eonas.de>
2016-04-07 11:14:15 +0200

    Fixed wrongly used clang argument "-o" in offline tests

--- testsuite/latest/offline_tests/fixed_opcode_generator_for_long_relocations/start_test.bat
+++ testsuite/latest/offline_tests/fixed_opcode_generator_for_long_relocations/start_test.bat
@@ -1,6 +1,6 @@
 @echo off
 cd test_sources
-%PATH_GNU_XPIC%\bin\clang -o0 -target xpic -Wimplicit  -T linker.ld main.s caller.s
+%PATH_GNU_XPIC%\bin\clang -o a.out -target xpic -Wimplicit  -T linker.ld main.s caller.s
 %PATH_GNU_XPIC%\bin\xpic-objdump -S a.out > objdump.txt
 fc objdump.org objdump.txt > null
 if %ERRORLEVEL%==0 (
--- testsuite/latest/offline_tests/long_relocation_linkererror_1_with_workaround/start_test.bat
+++ testsuite/latest/offline_tests/long_relocation_linkererror_1_with_workaround/start_test.bat
@@ -1,6 +1,6 @@
 @echo off
 cd test_sources
-%PATH_GNU_XPIC%\bin\clang -o0 -target xpic -Wimplicit  -T linker.ld main.c some.c any.c
+%PATH_GNU_XPIC%\bin\clang -o a.out -target xpic -Wimplicit  -T linker.ld main.c some.c any.c
 REM %PATH_GNU_XPIC%\bin\xpic-llvmc -O0 -Wa,-mmcu=xpic2 -T linker.ld main.c some.c any.c
 if %ERRORLEVEL%==0 (
 echo test was successful.
