5ded8195efd55ff39a321fc8b74ec7755993e586
gdormann <gdormann@hilscher.com>
2016-04-27 13:54:52 +0200

    Set xpic-compiler Version

--- llvm-3.4.2/lib/Target/xpic/xpicCompilerVersion.h
+++ llvm-3.4.2/lib/Target/xpic/xpicCompilerVersion.h
@@ -4,12 +4,12 @@
 
 #define SHOW_XPIC_BACKEND_VERSION
 
-#define XPIC_MAJOR	"0"
-#define XPIC_MINOR	"0"
-#define XPIC_RELEASE	"0"   // 0-alpha, 1-beta, 2-release candidate, 3-release,
+#define XPIC_MAJOR	"1"       // LLVM 3.4.2
+#define XPIC_MINOR	"0"       // first version
+#define XPIC_RELEASE	"0"   // internal version; not for customer
 
 #ifndef XPIC_BUILD
-#define XPIC_BUILD	"0 (private build)"   // 0-no bug fixes, 1, 2, 3 and so on - bug fixes
+#define XPIC_BUILD	"0"       // 0-no bug fixes, 1, 2, 3 and so on - bug fixes
 #endif
 
 #endif
