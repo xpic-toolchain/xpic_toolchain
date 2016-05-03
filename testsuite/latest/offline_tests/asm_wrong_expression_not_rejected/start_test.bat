b6cb6fed92dc04533f77d9562b4768d9a296a80a
gdormann <gdormann@hilscher.com>
2016-05-03 09:22:56 +0200

    remove compare output from showing at shell

--- testsuite/latest/offline_tests/asm_wrong_expression_not_rejected/start_test.bat
+++ testsuite/latest/offline_tests/asm_wrong_expression_not_rejected/start_test.bat
@@ -3,7 +3,7 @@ cd test_sources
 %PATH_GNU_XPIC%\bin\xpic-as -gdwarf2 test.s -o test.o 2> error.log
 
 REM return Error as long as wrong error is reported by xpic-as
-fc error.log error.log_ref
+fc error.log error.log_ref > NULL
 if %ERRORLEVEL%==0 (
 echo test was succeesful.
 
